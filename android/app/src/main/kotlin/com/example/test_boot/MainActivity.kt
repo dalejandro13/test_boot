a99edd43cc0eaac80a926cd1c2a6565de811e0d5
Andres Ruiz <rb.andres98@gmail.com>
2024-07-26 14:57:42 -0500

    Fix Auto Boot

--- android/app/src/main/kotlin/com/example/test_boot/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_boot/MainActivity.kt
@@ -4,20 +4,22 @@ import io.flutter.embedding.android.FlutterActivity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
-import android.util.Log
 
-class MainActivity: FlutterActivity(){
+class MainActivity : FlutterActivity() {
+    companion object {
+        const val REQUEST_OVERLAY_PERMISSIONS = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var REQUEST_OVERLAY_PERMISSIONS = 100
-        if (!Settings.canDrawOverlays(getApplicationContext())) {
+        if (!Settings.canDrawOverlays(this)) {
             val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            val uri: Uri = Uri.fromParts("package", getPackageName(), null)
-            myIntent.setData(uri)
+            val uri: Uri = Uri.fromParts("package", packageName, null)
+            myIntent.data = uri
             startActivityForResult(myIntent, REQUEST_OVERLAY_PERMISSIONS)
             return
         }
@@ -26,15 +28,11 @@ class MainActivity: FlutterActivity(){
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d("BootCompletedReceiver", "onReceive called")
-        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-            val activityIntent = Intent(context, MainActivity::class.java)
-            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(activityIntent)
-            Log.d("BootCompletedReceiver", "MainActivity started")
-        }
-        else{
-            Log.d("BootCompletedReceiver", "Received unexpected action: ${intent.action}")
+        val handler = Handler()
+        handler.post {
+            val dialogIntent = Intent(context, MainActivity::class.java)
+            dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(dialogIntent)
         }
     }
 }
